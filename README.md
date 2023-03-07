635c8087bde93e5dbb62ae6ccd412dbfe925956e
ddc-hust <1966423145@qq.com>
2023-03-07 16:27:53 +0800

    打包样式资源

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 ## 2. webpack配置文件
 1. 建立webpack.config.js文件，填写基本配置
 2. 使用npx webpack即可打包，效果同第一步一样
+
+## 3. 使用loader处理样式资源
+1. style-loader
+2. css-loader
+3. less-loader
+4. sass-loader
--- firstProjects/webpack.config.js
+++ firstProjects/webpack.config.js
@@ -6,7 +6,21 @@ module.exports = {
         filename: "main.js",
     },
     module: {
-        rules: [],
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+
+        ],
     },
     plugins: [],
     mode: 'development',
